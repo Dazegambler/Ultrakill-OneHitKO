2ef81005fa34642ffa9c7c6ac89ad3c9959dd3b2
Dazegambler <70617250+Dazegambler@users.noreply.github.com>
2021-07-02 22:11:14 -0300

    Add files via upload

---
+++ UKOHKO/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace TestMod
+{
+    [BepInPlugin("Test.Mod", "TESTMOD", "1.0.0")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        private ConfigEntry<KeyCode> Toggle;
+
+        GameObject Player;
+        bool toggle = false;
+
+        public void Start()
+        {
+            Toggle = Config.Bind("Binds", "OHKO toggle", KeyCode.L, "");
+        }
+        public void Update()
+        {
+            if(Player != null)
+            {
+                if (Input.GetKeyDown(Toggle.Value))
+                {
+                    toggle = !toggle;
+                }
+                if (toggle)
+                {
+                    var hp = Player.gameObject.GetComponent<NewMovement>();
+                    if (hp.hp > 1)
+                    {
+                        hp.hp = 1;
+                    }
+                }
+            }
+            ObjCheck();
+        }
+        private void ObjCheck()
+        {
+            if(Player == null)
+            {
+                Player = GameObject.Find("Player");
+            }
+        }
+    }
+}
