a5f95a50382de0d92938d5a7d5a1a169e38dc188
Dazegambler <70617250+Dazegambler@users.noreply.github.com>
2024-04-17 17:05:40 -0300

    Add files via upload
    Rework

---
+++ UKOHKO/UKOHKO.cs
@@ -0,0 +1,44 @@
+﻿namespace UKOHKO;
+
+using System.Security.AccessControl;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+[BepInPlugin("UKOHKO","UKOHKO","2.0")]
+public class Plugin:BaseUnityPlugin
+{
+    ConfigEntry<KeyCode> ohkokey;
+
+    ConfigEntry<bool> ohkostart;
+
+    NewMovement Player;
+
+    bool ohko = false;
+
+    void Start(){
+        Debug.Log("UKOHKO Loaded");
+        ohkokey = Config.Bind("Binds","OHKO toggle", KeyCode.O, "Key to toggle One hit ko");
+        ohkostart = Config.Bind("Misc","Start with OHKO enabled",false,"");
+        ohko = ohkostart.Value;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(ohkokey.Value)){
+            ohko = !ohko;
+            Debug.Log("ohko:"+ohko);
+        }
+        if(ohko){
+            if(!Player){
+                GetPlayer().hp = Mathf.Clamp(Player.hp,0,1);
+                return;
+            }
+            Player.hp = Mathf.Clamp(Player.hp,0,1);
+        }       
+    }
+
+    NewMovement GetPlayer(){
+        Player = NewMovement.Instance;
+        return Player;
+    }
+}
