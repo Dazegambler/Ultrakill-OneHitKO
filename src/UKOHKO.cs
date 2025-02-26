9016a2eedc012bfe4cfb9a756c5140cd1d8e4827
Dazegambler <jadir689@gmail.com>
2025-02-26 16:16:17 -0300

    can't use default config library now

--- src/UKOHKO.cs
+++ src/UKOHKO.cs
@@ -4,12 +4,11 @@ using BepInEx;
 using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 [BepInPlugin("UKOHKO","UKOHKO","2.0")]
 public class Plugin:BaseUnityPlugin
 {
-    ConfigEntry<KeyCode> ohkokey;
-
     ConfigEntry<bool> ohkostart;
 
     NewMovement Player;
@@ -18,7 +17,6 @@ public class Plugin:BaseUnityPlugin
 
     void Start(){
         Debug.Log("UKOHKO Loaded");
-        ohkokey = Config.Bind("Binds","OHKO toggle", KeyCode.O, "Key to toggle One hit ko");
         ohkostart = Config.Bind("Misc","Start with OHKO enabled",false,"");
         ohko = ohkostart.Value;
     }
